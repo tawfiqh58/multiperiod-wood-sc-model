0d2241ff24adfe80ccb5bf69c6dc613ff13e8f27
tawfiq58 <tawfiqhasan58@gmail.com>
2022-09-30 20:24:52 +0600

    Read all parameters

---
+++ GA/src/parameters/cd.py
@@ -0,0 +1,23 @@
+import utils
+from openpyxl import load_workbook
+doc = load_workbook(filename='cd.xlsx')
+
+# 3 level nesting
+sn = doc.sheetnames
+data = []
+
+for sheet in sn:
+    ws = doc[str(sheet)]
+
+    # init array
+    _data = [[0 for _ in range(2, ws.max_column+1)]
+        for _ in range(2, ws.max_row+1)]
+
+    # assign values
+    for i in range(2, ws.max_row+1):
+        for j in range(2, ws.max_column+1):
+            _data[i-2][j-2] = ws.cell(i, j).value
+    data.append(_data)
+d = utils.convert_to_tuple(data)
+print(d) # ([[1, 2], [3, 4]], [[2, 3], [4, 5]])
+# print(type(utils.convert_to_tuple(data)))
---
+++ GA/src/parameters/constants.py
@@ -0,0 +1,15 @@
+import utils
+from openpyxl import load_workbook
+doc = load_workbook(filename='constants.xlsx')
+ws = doc.active
+
+data = [[0 for _ in range(2, ws.max_column+1)]
+    for _ in range(2, ws.max_row+1)]
+
+# assign values
+for i in range(2, ws.max_row+1):
+    for j in range(2, ws.max_column+1):
+        data[i-2][j-2] = ws.cell(i, j).value
+
+d = utils.convert_to_tuple(data)
+print(d) # ([100], [10000000], [1000000000], [12], [8000], [8000])
